3e97358d016995d3cda4509ae28d023f2c949b6f
BestStepInUa <beststep.in.ua@gmail.com>
2023-11-09 13:17:22 +0200

    minor

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,9 @@ const BASE_API_URL = 'https://books-backend.p.goit.global/books';
 
 const Refs = {
     categoriesList: document.querySelector('.categories-list'),
+    categoriesListItemsLinks: document.querySelectorAll('.categories-list-item-link'),
     bookCaregoriesContainer: document.querySelector('.book-categories-container'),
-    bookCardsList: document.querySelector('.book-cards-list')
+    bookCardsList: document.querySelector('.book-cards-list')    
 }
 
 getCategories()
@@ -27,7 +28,7 @@ getTopBooks()
     .catch((err) => {
         console.error(err);        
         // Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    }) 
+    })
 
 async function getCategories() {        
     return await axios.get(`${BASE_API_URL}/category-list`)
@@ -49,12 +50,32 @@ async function getTopBooks() {
         });
 }
 
+// async function getBooksInCategory(category) {
+//     return await axios.get(`${BASE_API_URL}/category?category=${category}`)
+//         .then(resp => {
+//             if (!resp.status) {
+//                 throw new Error(resp.status || resp.statusText);
+//             }
+//             return resp.data;                                             
+//         });
+// }
+
+function onLoadCategory() {
+    // const category = evt.currentTarget;
+    console.log('Click');
+
+}
+
 function createCategoriesListMarkup(categories) {
     const result = categories.map(
         ({ list_name }) =>
-            `<li class="categories-list-item">${list_name}</li>`
+            `<li class="categories-list-item" data-category-name="${list_name}">
+                <a href="#">${list_name}</a>
+            </li>`
     ).sort((a,b) => a.localeCompare(b));    
-    result.unshift(`<li class="categories-list-item active">All categories</li>`);
+    result.unshift(`<li class="categories-list-item active" data-category-name="All categories">
+        <a href="#">All categories</a>
+    </li>`);
     return result.join('');
 }
 
